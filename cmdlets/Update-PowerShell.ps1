d9f4e9115354b243816818594161b83b544ecdf3
CircuitSpells <CircuitSpells@gmail.com>
2023-11-01 23:51:10 -0700

    added script

---
+++ cmdlets/Update-PowerShell.ps1
@@ -0,0 +1,13 @@
+function Update-PowerShell {
+    [CmdletBinding()]
+    param ()
+    
+    process {
+        if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            Write-Host "This script must be run as an administrator"
+            return
+        }
+
+        & winget upgrade --id Microsoft.Powershell
+    }
+}
