4bed9876f1d0bee3cd953daa3cec0109819fba59
CircuitSpells <CircuitSpells@gmail.com>
2023-11-02 00:18:36 -0700

    added script

---
+++ cmdlets/Install-PowerShell.ps1
@@ -0,0 +1,11 @@
+function Install-PowerShell {
+    [CmdletBinding()]
+    param()
+
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "This script must be run as an administrator"
+        return
+    }
+
+    winget install Microsoft.PowerShell -e
+}
