9ac99cef72b0f9ed57dc13544a0235c2252f38d1
CircuitSpells <CircuitSpells@gmail.com>
2023-11-01 23:25:20 -0700

    added Recurse parameter

--- cmdlets/Rename-AllItemsWithPrefix.ps1
+++ cmdlets/Rename-AllItemsWithPrefix.ps1
@@ -2,10 +2,11 @@ function Rename-AllItemsWithPrefix {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)][string]$Path,
-        [Parameter(Mandatory=$true)][string]$Prefix
+        [Parameter(Mandatory=$true)][string]$Prefix,
+        [Parameter(Mandatory=$false)][switch]$Recurse
     )
     
     process {
-        Get-ChildItem -Path $Path -File | Rename-Item -NewName { $Prefix + $_.Name }
+        Get-ChildItem -Path $Path -File -Recurse:$Recurse | Rename-Item -NewName { $Prefix + $_.Name }
     }
 }
