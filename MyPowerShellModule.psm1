1336da2b9f4a5d3a4bfc9916e041bacc38b0b159
CircuitSpells <CircuitSpells@gmail.com>
2023-11-01 23:11:22 -0700

    Created README

--- MyPowerShellModule.psm1
+++ MyPowerShellModule.psm1
@@ -1,6 +1,3 @@
-# usage: place the following line in 'C:\Users\<UserName>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1':
-# Import-Module "<PathToRepo>\PowershellScripts\MyPowerShellModule.psm1" -Force
-
 $PathToCmdlets = $PSScriptRoot + '\cmdlets\'
 Get-ChildItem -Path $PathToCmdlets -Filter *.ps1 | ForEach-Object {
     Get-Command -Name * -Module (Import-Module -Name $_.FullName -Force -PassThru) -CommandType Cmdlet
