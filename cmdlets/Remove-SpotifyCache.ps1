dee0f32c147bbb15d04e775812428ba7ddcf31ef
CircuitSpells <CircuitSpells@gmail.com>
2023-11-02 23:48:45 -0700

    improved parameter default

--- cmdlets/Remove-SpotifyCache.ps1
+++ cmdlets/Remove-SpotifyCache.ps1
@@ -1,9 +1,9 @@
 function Remove-SpotifyCache {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$false)][string]$UserName = 'Drake'
+        [Parameter(Mandatory=$false)][string]$UserName = $env:USERNAME
     )
 
     Get-ChildItem "C:\Users\$UserName\AppData\Local\Spotify\Data\" | Remove-Item  -Recurse -Force
-    Write-Host "Spotify cache deleted..."
+    Write-Host "Spotify cache deleted"
 }
