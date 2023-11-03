3390ff888fc0d34b70e0bd4dd1b9d189344e7757
CircuitSpells <CircuitSpells@gmail.com>
2023-11-02 22:02:36 -0700

    removed line

--- scripts/Rename-FilesWithCustomCriteria.ps1
+++ scripts/Rename-FilesWithCustomCriteria.ps1
@@ -8,7 +8,6 @@ function Rename-FilesWithCustomCriteria {
         [Parameter(Mandatory=$true)][string]$Path
     )
   
-    Get-ChildItem -Path $Path -Recurse -File | Rename-Item -NewName { $_.Name -Replace "Parks.and.Recreation.", "" }
     Get-ChildItem -Path $Path -Recurse -File | Rename-Item -NewName { $_.Name -Replace ".1080p.WEB-DL.x265.10bit.AAC.5.1-ImE\[UTR\].mkv", "" }
     Get-ChildItem -Path $Path -Recurse -File | Rename-Item -NewName { $_.Name -Replace "\.", " " }
     Get-ChildItem -Path $Path -Recurse -File | Rename-Item -NewName { $_.Name + ".mkv" }        
