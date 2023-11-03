88ca9afc0e42bd00604ba0b98f600ee1315eb0f8
CircuitSpells <CircuitSpells@gmail.com>
2023-11-03 00:53:36 -0700

    renamed startup module

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/my-theme.omp.json" | Invoke-Expression
-Import-Module "<PathToRepo>\PowerShellScripts\MyPowerShellModule.psm1" -Force
+Import-Module "<PathToRepo>\PowerShellScripts\StartupModule.psm1" -Force
 Set-Alias -Name clera -Value Clear-Host
 Set-Alias -Name gh -Value Get-Help
