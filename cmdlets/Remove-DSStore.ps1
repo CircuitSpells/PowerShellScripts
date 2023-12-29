89f90bdee86b1aab555c9b316155e7ba5254f5bb
CircuitSpells <CircuitSpells@gmail.com>
2023-12-29 14:04:48 -0800

    added script

---
+++ cmdlets/Remove-DSStore.ps1
@@ -0,0 +1,13 @@
+<#
+.DESCRIPTION
+Removes all .DS_Store files in a directory.
+#>
+function Remove-DSStore {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)][string]$Path,
+        [Parameter(Mandatory=$false)][switch]$Recurse
+    )
+    
+    Get-ChildItem -Path $Path -File -Recurse:$Recurse | Where-Object {$_.Name -eq ".DS_Store"} | Remove-Item -Force
+}
