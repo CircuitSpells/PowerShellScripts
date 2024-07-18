fe98af457bdf3851e9a6450ce9de8a46dc016e07
CircuitSpells <CircuitSpells@gmail.com>
2024-07-17 21:35:28 -0700

    fix: added variable to path

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@
 $PathToRepo = "C:\Path\To\PowershellScripts"
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/my-theme.omp.json" | Invoke-Expression
-Import-Module "<PathToRepo>\PowerShellScripts\StartupModule.psm1" -Force
+Import-Module "$PathToRepo\PowerShellScripts\StartupModule.psm1" -Force
 Set-Alias -Name clera -Value Clear-Host
 Set-Alias -Name gh -Value Get-Help
 
